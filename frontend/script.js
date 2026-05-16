cb13b1f6c2fffbdd523cf2f42ec140dfcbcbd4fc
venkatanagasrilakshmir-eng <venkatanagasrilakshmir@gmail.com>
2026-05-17 04:21:26 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
 let result = document.getElementById("result");
+let body = document.body;
+let modeBtn = document.getElementById("modeBtn");
 
 function append(value) {
   result.value += value;
@@ -13,5 +15,16 @@ function calculate() {
     result.value = eval(result.value);
   } catch {
     result.value = "Error";
+  }
+}
+
+/* 🌙 Toggle Dark / Light Mode */
+function toggleMode() {
+  body.classList.toggle("light");
+
+  if (body.classList.contains("light")) {
+    modeBtn.innerText = "☀ Light";
+  } else {
+    modeBtn.innerText = "🌙 Dark";
   }
 }
